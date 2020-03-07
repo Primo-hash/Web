1424eb58869b5ba86f5017319a34cfee0a9c3b19
Primo-hash <61516691+Primo-hash@users.noreply.github.com>
2020-03-07 21:23:56 +0100

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import image from 'https://github.com/Primo-hash/Web/master/src/Assets/profile.jpg';
+import image from 'https://github.com/Primo-hash/Web/tree/master/src/Assets/profile.jpg';
 
 class Sidebar extends Component {
   constructor(props) {
